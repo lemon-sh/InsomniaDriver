f1df7eec92b89a81bcd2b28976727966838b315b
lemonsh <lemon@lemonsh.moe>
2022-12-17 19:54:44 +0100

    dźwięk alarmu i inne takie

--- master/config.py
+++ master/config.py
@@ -1,5 +1,9 @@
+# -- DROWSY THRESHOLDS
+
 ear_threshold = 0.18
 wait_time_threshold = 1.0
+
+# -- FONT
 
 # 'sys' (system font) or 'file' (eg. ttf file)
 # any other value = default font
@@ -8,17 +12,26 @@ font_size = 24
 font_antialias = True
 font = "CozetteVector.ttf"
 
+# -- COLORS
+
 red = (255, 50, 50)
-green = (50, 255, 50)
-blue = (50, 50, 255)
+green = (100, 255, 100)
+blue = (80, 80, 255)
 
 white = (250, 250, 250)
 gray = (100, 100, 100)
 shadow = (10, 10, 10)
 black = (0, 0, 0)
 
+# -- ALARM
+
+alarm_sound = "alarm.ogg"
+
+# -- VIDEO MODES
+
 # first is the highest priority, last is the lowest
 # if no mode from this list maches, first is the default
 supported_video_modes = [
-    (1280, 720)
+    # (1280, 720),
+    (800, 480)
 ]
--- master/main.py
+++ master/main.py
@@ -13,17 +13,6 @@ import graphics
 
 print(f"MediaPipe {mp.__version__}\nOpenCV {cv2.__version__}\n")
 
-if sys.platform == "win32":
-    print("Windows detected, using DirectShow OpenCV backend")
-    cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)
-elif sys.platform == "linux":
-    print("Linux detected, using V4L2 OpenCV backend")
-    cap = cv2.VideoCapture(0, cv2.CAP_V4L2)
-else:
-    print("Unknown OS detected, using default OpenCV backend")
-    cap = cv2.VideoCapture(0)
-
-drowsy_detector = detector.DrowsyDetector()
 print("Intializing display...")
 pygame.init()
 pygame.display.init()
@@ -60,26 +49,52 @@ title_text = "InsomniaDriver"
 title_size_x, title_size_y = title_font.size(title_text)
 title_x = screen_x / 2 - title_size_x / 2
 title_y = screen_y / 2 - title_size_y / 2
+
 sub_text = "Paweł Stolarski & Jakub Piasecki"
 sub_size_x, sub_size_y = font.size(sub_text)
-sub_x = title_x+title_size_x-sub_size_x
-sub_y = title_y+title_size_y
+sub_x = title_x + title_size_x - sub_size_x
+sub_y = title_y + title_size_y
+
+init_text = "Wczytywanie..."
+init_size_x, init_size_y = font.size(init_text)
+init_x = screen_x/2 - init_size_x/2
+init_y = sub_y+init_size_y+40
 
 screen.fill(config.shadow)
 graphics.draw_text(screen, title_text, title_font, config.white, config.black, (title_x, title_y))
 graphics.draw_text(screen, sub_text, font, config.gray, config.black, (sub_x, sub_y))
+graphics.draw_text(screen, init_text, font, config.green, config.black, (init_x, init_y))
 pygame.display.flip()
-time.sleep(10)
 
-fpsf_width, fpsf_height = font.size("FPS: 999.9")
-fpsf_pos = (mode[0] - fpsf_width - 20, mode[1] - fpsf_height - 20)
-fps = 0
+if sys.platform == "win32":
+    print("Windows detected, using DirectShow OpenCV backend")
+    cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)
+elif sys.platform == "linux":
+    print("Linux detected, using V4L2 OpenCV backend")
+    cap = cv2.VideoCapture(0, cv2.CAP_V4L2)
+else:
+    print("Unknown OS detected, using default OpenCV backend")
+    cap = cv2.VideoCapture(0)
 
 cap.set(cv2.CAP_PROP_FRAME_WIDTH, screen_x)
 cap.set(cv2.CAP_PROP_FRAME_HEIGHT, screen_y)
 
+fpsf_width, fpsf_height = font.size("FPS: 999.9")
+flash_pos = (20, 20)
+stats_pos = (20, screen_y - fpsf_height - 20)
+# alarm_pos = (20, stats_pos[1]-fpsf_height-10)
+fpsf_pos = (screen_x - fpsf_width - 20, screen_y - fpsf_height - 20)
+
+pygame.mixer.music.load(config.alarm_sound)
+pygame.mixer.music.play(-1)
+pygame.mixer.music.pause()
+
+drowsy_detector = detector.DrowsyDetector()
+fps = 0
 while True:
     fpsc_start = time.perf_counter()
+    pygame.event.pump()
+
     ret, image = cap.read()
     if not ret:
         break
@@ -87,12 +102,30 @@ while True:
     pgimage = graphics.cvimage_to_pygame(image)
     screen.fill((0, 0, 0))
     screen.blit(pgimage, pgimage.get_rect())
+
+    alert = pygame.mixer.music.get_busy()
     if drowsy_detector.detection:
         graphics.draw_eye_landmarks(screen, drowsy_detector.coordinates[0], drowsy_detector.coordinates[1], config.blue)
+        ear = round(drowsy_detector.ear, 3)
+        drowsy_time = round(drowsy_detector.drowsy_time, 3)
+        graphics.draw_text(screen, f"EAR: {ear}; Przysypianie: {drowsy_time}", font, config.blue, config.shadow,
+                           stats_pos)
+        if drowsy_detector.alarm and not alert:
+            pygame.mixer.music.set_pos(0)
+            pygame.mixer.music.unpause()
+            alert = True
+
+        if alert:
+            graphics.draw_text(screen, "Alert!", font, config.red, config.shadow, flash_pos)
+        else:
+            graphics.draw_text(screen, "Gotowość.", font, config.green, config.shadow, flash_pos)
     else:
-        graphics.draw_text(screen, "Nie wykryto mordy", font, config.red, config.shadow, (20, 20))
-    fps_text = f"FPS: {fps}"
-    graphics.draw_text(screen, fps_text, font, config.white, config.shadow, fpsf_pos)
+        graphics.draw_text(screen, "Nie wykryto mordy", font, config.red, config.shadow, flash_pos)
+
+    if pygame.key.get_pressed()[pygame.K_x] and alert:
+        pygame.mixer.music.pause()
+
+    graphics.draw_text(screen, f"FPS: {fps}", font, config.white, config.shadow, fpsf_pos)
     pygame.display.flip()
     fpsc_stop = time.perf_counter()
     fps = round(1 / (fpsc_stop - fpsc_start), 1)
